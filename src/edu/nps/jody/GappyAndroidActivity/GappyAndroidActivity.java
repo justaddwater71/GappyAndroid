7ba226da7963d47d93d84efe0f5a5d132373eed9
Jody <justaddwater71@gmail.com>
2010-06-11 15:28:49 -0700

    Very ambitious attempt to read a file in when the file browser button is used vice the path button.  This gives me the "find directory" and "find file" functionality from separate buttons.

--- src/edu/nps/jody/GappyAndroidActivity/GappyAndroidActivity.java
+++ src/edu/nps/jody/GappyAndroidActivity/GappyAndroidActivity.java
@@ -1,6 +1,10 @@
 package edu.nps.jody.GappyAndroidActivity;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -25,13 +29,16 @@ public class GappyAndroidActivity extends Activity
 	//Data Members
 		//GappyAndroid
 		String		 filePath = "/";
-		String		 fileName = "";
 		EditText 	path;
+		
+		String		 fileName = "";
 		EditText	 file;
+		
+		String fileContents = "";
 		TextView fileView;
 		
 		//guiBrowse
-		File 			currentDirectory = new File("/proc");
+		File 			currentDirectory = new File(filePath);
 		ListView listView;
 		TextView browsePath;
 	
@@ -68,25 +75,27 @@ public class GappyAndroidActivity extends Activity
     //Methods
     private void mainView()
     {
-        setContentView(R.layout.main);
-        
-		path		= (EditText)findViewById(R.id.path);
-Button		pathGo		= (Button)findViewById(R.id.pathGo);
-Button		pathBrowse	= (Button)findViewById(R.id.pathBrowse);
-
-		file		= (EditText)findViewById(R.id.file);
-Button		fileGo		= (Button)findViewById(R.id.fileGo);
-Button		fileBrowse	= (Button)findViewById(R.id.fileBrowse);
-
-		fileView	= (TextView)findViewById(R.id.fileView);
-
-path.setText(filePath);
-pathGo.setOnClickListener(onPathGoClick);
-pathBrowse.setOnClickListener(onPathBrowse);
-
-file.setText(fileName);
-fileGo.setOnClickListener(onFileGoClick);
-fileBrowse.setOnClickListener(onFileBrowse);
+    		setContentView(R.layout.main);
+			        
+					path		= (EditText)findViewById(R.id.path);
+			Button		pathGo		= (Button)findViewById(R.id.pathGo);
+			Button		pathBrowse	= (Button)findViewById(R.id.pathBrowse);
+			
+					file		= (EditText)findViewById(R.id.file);
+			Button		fileGo		= (Button)findViewById(R.id.fileGo);
+			Button		fileBrowse	= (Button)findViewById(R.id.fileBrowse);
+			
+					fileView	= (TextView)findViewById(R.id.fileView);
+			
+			path.setText(filePath);
+			pathGo.setOnClickListener(onPathGoClick);
+			pathBrowse.setOnClickListener(onPathBrowse);
+			
+			file.setText(fileName);
+			fileGo.setOnClickListener(onFileGoClick);
+			fileBrowse.setOnClickListener(onFileBrowse);
+			
+			fileView.setText(fileContents);
     }
     
     private Button.OnClickListener onPathBrowse = new Button.OnClickListener()
@@ -95,7 +104,10 @@ fileBrowse.setOnClickListener(onFileBrowse);
 		public void onClick(View view) 
 		{
 			//Launch FileBrowser as another view vice a new Activity, pass parameter to only allow directories
-			guiBrowse();
+			filePath = path.getText().toString();
+			fileName = file.getText().toString();
+			hideKeyboard(view);
+			guiBrowse(false);
 		}
     	
     };
@@ -103,10 +115,12 @@ fileBrowse.setOnClickListener(onFileBrowse);
     private Button.OnClickListener onFileBrowse = new Button.OnClickListener()
     {
 
-		public void onClick(View v) 
+		public void onClick(View view) 
 		{
 			//Launch FileBrowser as another view vice a new Activity, pass parameter to only allow files
-			guiBrowse();
+			hideKeyboard(view);
+			
+			guiBrowse(true);
 		}
     	
     };
@@ -117,14 +131,8 @@ fileBrowse.setOnClickListener(onFileBrowse);
 		{	
 			filePath = path.getText().toString();
 			fileName = file.getText().toString();
-			path.getContext();
 			
-			/* Below hide-keyboard code copied from
-			 * http://stackoverflow.com/questions/1109022/how-to-close-hide-the-android-soft-keyboard
-			 */
-			InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(path.getApplicationWindowToken(), 0);
-            //End copy from stackoverflow
+			hideKeyboard(path);
             
 			fileView.setText(filePath + "/" + fileName);
 		}
@@ -137,35 +145,35 @@ fileBrowse.setOnClickListener(onFileBrowse);
 		{
 			filePath = path.getText().toString();
 			fileName = file.getText().toString();
-			file.getContext();
 			
-			/*
-			 * Below hide-keyboard code copied from
-			 * http://stackoverflow.com/questions/1109022/how-to-close-hide-the-android-soft-keyboard
-			 */
-			InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(file.getApplicationWindowToken(), 0);
-            //End copy from stackoverflow
-            
+			hideKeyboard(file);
+
 			fileView.setText(filePath + "/" + fileName);
 		}
     	
     };
     
-    private void guiBrowse()
+    private void guiBrowse(boolean openFIle)
     {
     		setContentView(R.layout.browser);
     		
     		//Create a list view, populate it with an array of files, display the view and listen for click
             listView = (ListView)findViewById(R.id.listView);
             
-            File[] files= new File("/proc").listFiles();
+            File[] files= new File(filePath).listFiles();
             
             String[] fileNames = dropPath(files);
             
             listView.setAdapter(new ArrayAdapter<String>(this, R.layout.file_row, fileNames));
             
-            listView.setOnItemClickListener(onListClick);
+            if (openFIle)
+            {
+            	listView.setOnItemClickListener(onOpenListClick);
+            }
+            else
+            {
+            	listView.setOnItemClickListener(onListClick);
+            }
             
             //If the user needs to go "up" the directory, here is the button to do it
             Button up = (Button)findViewById(R.id.up);
@@ -176,7 +184,7 @@ fileBrowse.setOnClickListener(onFileBrowse);
             
             //Keep updating the address location
             browsePath = (TextView)findViewById(R.id.browsePath);
-            browsePath.setText("/proc");
+            browsePath.setText(filePath);
             
     }
     
@@ -314,5 +322,136 @@ fileBrowse.setOnClickListener(onFileBrowse);
 		
 		return fileOnly;
 	}
+	
+	private void hideKeyboard(View view)
+	{
+		/*
+		 * Below hide-keyboard code copied from
+		 * http://stackoverflow.com/questions/1109022/how-to-close-hide-the-android-soft-keyboard
+		 */
+		InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getApplicationWindowToken(), 0);
+        //End copy from stackoverflow
+	}
     
+	private OnItemClickListener onOpenListClick = new OnItemClickListener()
+	{
+		public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+		{
+				browseOpenTo(position);
+		}
+	};
+	
+	private void browseOpenTo(int position)
+	{	
+		String fileName = (String)listView.getItemAtPosition(position);
+		String currentLine ="";
+		
+		
+		File tempFile = new File(currentDirectory.getAbsolutePath() + "/" + fileName);
+		
+		if (tempFile.isDirectory())
+		{
+			if (tempFile.canRead())
+			{
+				File[] files = tempFile.listFiles();
+				currentDirectory = tempFile;
+				browsePath.setText(currentDirectory.getAbsolutePath());
+				
+				if (files == null)
+				{
+					listView.setAdapter(new ArrayAdapter<String>(this, R.layout.file_row, dropPath(files)));
+				}
+				else
+				{
+					listView.setAdapter(new ArrayAdapter<String>(this, R.layout.file_row, dropPath(files)));
+				}
+			}
+			else
+			{
+				OnClickListener fileButtonListener = new OnClickListener()
+				{
+					//@Override
+					public void onClick(DialogInterface arg0, int arg1) 
+					{
+						//Do nothing
+					}
+				};
+				
+				new AlertDialog.Builder(this)
+					.setTitle("Security Notice")
+					.setMessage("Permission Denied")
+					.setPositiveButton("OK", fileButtonListener)
+					.show();
+			}
+
+		}
+		else
+		{
+			/*OnClickListener fileButtonListener = new OnClickListener()
+			{
+				//@Override
+				public void onClick(DialogInterface arg0, int arg1) 
+				{
+					//Do nothing
+				}
+			};
+			
+			new AlertDialog.Builder(this)
+				.setTitle("File Selected!")
+				.setMessage("That is not a directory")
+				.setPositiveButton("OK", fileButtonListener)
+				.show();*/
+				
+				try 
+				{
+					FileReader fileReader = new FileReader(tempFile);
+					BufferedReader bufferedReader = new BufferedReader(fileReader);
+					
+					fileContents="";
+					
+					while ((currentLine = bufferedReader.readLine()) != null)
+					{
+						fileContents.concat(currentLine);
+					}
+					
+				} 
+				catch (FileNotFoundException e) 
+				{
+					OnClickListener fileButtonListener = new OnClickListener()
+					{
+						//@Override
+						public void onClick(DialogInterface arg0, int arg1) 
+						{
+							//Do nothing
+						}
+					};
+					
+					new AlertDialog.Builder(this)
+					.setTitle("Error!")
+					.setMessage("FIle Not Found")
+					.setPositiveButton("OK", fileButtonListener)
+					.show();
+					//e.printStackTrace();
+				}
+				catch (IOException i)
+				{
+					OnClickListener fileButtonListener = new OnClickListener()
+					{
+						//@Override
+						public void onClick(DialogInterface arg0, int arg1) 
+						{
+							//Do nothing
+						}
+					};
+					
+					new AlertDialog.Builder(this)
+					.setTitle("Error!")
+					.setMessage("IO Error")
+					.setPositiveButton("OK", fileButtonListener)
+					.show();
+				}
+		}
+	}
+	
 }
